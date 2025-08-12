4e7a5399ea1961e3dbc343a0d081d1ae940c1d1a
SoftronicsDevOps <softronics.llp@gmail.com>
2025-08-12 22:14:20 +0530

    after slam in sim mode

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -19,8 +19,22 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
-    )
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+            )
+    
+    joystick = IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource([os.path.join(
+                        get_package_share_directory(package_name),'launch','joystick.launch.py'
+                    )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+                    package="twist_mux",
+                    executable="twist_mux",
+                    parameters=[twist_mux_params, {'use_sim_time': True}],
+                    remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+                )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -66,6 +80,8 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
+        twist_mux,
         gazebo,
         spawn_entity,
         delayed_diff_drive_spawner,
